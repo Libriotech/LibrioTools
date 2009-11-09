13a3e37298bb42176024461b7a695813d0a49737
MagnusEnger <magnus@enger.priv.no>
2009-11-09 16:29:25 -0500

    Improved version of oneoff/fixitems.pl

--- oneoff/fixitems.pl
+++ oneoff/fixitems.pl
@@ -11,37 +11,51 @@ my $rqbiblios=$dbh->prepare("SELECT biblionumber from biblioitems");
 $rqbiblios->execute;
 $|=1;
 my $counter = 0;
-while (my ($biblionumber)= $rqbiblios->fetchrow_array){
+my $item_count = 0;
+RECORD: while (my ($biblionumber)= $rqbiblios->fetchrow_array){
 
     my $record=GetMarcBiblio($biblionumber);
 	
-	# Only looking for 942$a = skskb
-	if ($record->field('942') && $record->field('942')->subfield('a') && $record->field('942')->subfield('a') ne 'skskb') {
-		next;	
-	} 
+	if ($record && $record->field('952')) {
 	
-	$counter++;
-	print "$counter $biblionumber\n";
+		# Loop through the items
+	    foreach my $field952 ($record->field('952')) {
 	
-	# Loop through the items
-    foreach my $field952 ($record->field('952')) {
-    	
-    	my $barcode = $field952->subfield('p');
-    	my $itemnumber = GetItemnumberFromBarcode($barcode);
-	    print "\t$barcode - $itemnumber\n";
+			# Only looking for 952$a = skskb
+			if ($field952 && $field952->subfield('a') && $field952->subfield('a') ne 'skskb') {
+				next RECORD;
+			} 
+	    	
+	    	my $barcode = $field952->subfield('p');
+	    	my $itemnumber = GetItemnumberFromBarcode($barcode);
+		    print "\t$barcode - $itemnumber\n";
+		    
+	   		# Change 952$c from IKT to BOKL
+			$field952->update(c => 'BOKL');
+			
+			# Add 952$y = LRM
+			$field952->add_subfields('y' => 'LRM');
+			
+			my $marcitem=MARC::Record->new();
+			$marcitem->encoding('UTF-8');
+			$marcitem->append_fields($field952);
+
+			eval{ModItemFromMarc($marcitem, $biblionumber, $itemnumber);};
+			if ($@){
+				warn "$biblionumber : $@";
+				# warn $record->as_formatted;
+			}
+			
+			$item_count++;
+		    
+	    }
 	    
-   		# Change 952$c from IKT to BOKL
-		$field952->update(c => 'BOKL');
-		
-		# Add 952$y = LRM
-		$field952->add_subfields('y' => 'LRM');
+		$counter++;
+		print "Count: $counter - Biblionumber: \n";
 	    
-    }
-    
-	# eval{ModItemFromMarc($record,$biblionumber,$itemfield->subfield('9'));};
-	# if ($@){
-	# 	warn "$biblionumber : $@";
-	# 	warn $record->as_formatted;
-	# }    
+	}
 
 }
+
+print "Record count: $counter\n";
+print "Item count:   $item_count\n";
