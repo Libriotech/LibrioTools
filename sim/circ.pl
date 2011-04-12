2a326cd8848bb8d589b4b1605fb483bc273c7d4e
Magnus Enger <magnus@enger.priv.no>
2011-04-12 12:00:24 +0200

    More work on circ.pl - still not working

--- sim/circ.pl
+++ sim/circ.pl
@@ -1,6 +1,5 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl 
 
-# circ.pl
 # Copyright 2011 Magnus Enger Libriotech
 #
 # This is free software; you can redistribute it and/or modify
@@ -17,6 +16,7 @@
 # along with this file; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+use C4::Circulation;
 use C4::Context;
 use C4::Members;
 use Date::Calc qw( Add_Delta_Days Day_of_Week Delta_Days Today );
@@ -24,13 +24,18 @@ use Getopt::Long;
 use Pod::Usage;
 use Data::Dumper;
 use strict;
+# use warnings;
+# use diagnostics;
 
 ## get command line options
-my ($from, $max, $min, $verbose, $debug) = get_options();
+my ($from, $max, $min, $returns_after, $verbose, $debug) = get_options();
 print "\nStarting circ.pl\nSettings:\n"      if $verbose;
 print "Simulating circulations from $from\n" if $verbose;
 print "Min number of issues per day: $min\n" if $verbose;
 print "Max number of issues per day: $max\n" if $verbose;
+
+# Number of returns to do per day, average of $min and $max
+my $number_of_returns = ($max + $min) / 2;
 
 my $dbh   = C4::Context->dbh();
 
@@ -51,9 +56,10 @@ my @stop  = Today();
 my $j = Delta_Days(@start,@stop);
 
 # Prepare some statement handles
-my $get_borrowers_sth = $dbh->prepare("SELECT borrowernumber FROM borrowers ORDER BY RAND() LIMIT ?");
+my $get_borrowers_sth = $dbh->prepare("SELECT cardnumber FROM borrowers WHERE cardnumber IS NOT NULL ORDER BY RAND() LIMIT ?");
 my $get_barcodes_sth  = $dbh->prepare("SELECT barcode FROM items WHERE onloan IS NULL ORDER BY RAND() LIMIT ?");
 
+DATES:
 for ( my $i = 0; $i <= $j; $i++ ) {
 
   # Calculate the dates
@@ -63,6 +69,7 @@ for ( my $i = 0; $i <= $j; $i++ ) {
   my $date = $date[0] . "-" . $date[1] . "-" . $date[2];
 
   # Skip sundays
+  # TODO Turn this into an option
   if (Day_of_Week(@date) == 7) {
     print "$date Skipping Sunday\n";
     next;
@@ -70,34 +77,43 @@ for ( my $i = 0; $i <= $j; $i++ ) {
 
   # Find the number of issues we want to do
   my $issues_to_do = int(rand($max-$min+1)) + $min;
-  if ($verbose) { print "$date Going to do $issues_to_do issues\n"; }
+  if ($verbose) { print "Day #$i $date Going to do $issues_to_do issues\n"; }
 
   # Get the borrowernumbers
   $get_borrowers_sth->execute($issues_to_do);
-  while (my $borrower = $get_borrowers_sth->fetchrow_hashref()) {
-    if ($debug) { print "\tBorrower: " . $borrower->{borrowernumber} . "\n"; }
-  }
+  while (my $cardnumber = $get_borrowers_sth->fetchrow_hashref()) {
+    
+    # Get the barcode of a random item that is not on loan
+    $get_barcodes_sth->execute(1);
+    my $barcode = $get_barcodes_sth->fetchrow_hashref();
+    if ($debug) { print "\$barcode ", Dumper $barcode; }
   
-  # Get the barcodes
-  $get_barcodes_sth->execute($issues_to_do);
-  while (my $barcode = $get_barcodes_sth->fetchrow_hashref()) {
-    if ($debug) { print "\tBarcode: " . $barcode->{barcode} . "\n"; }
+    my $borrower = GetMember( 'cardnumber' => $cardnumber->{'cardnumber'} );
+    if ($debug) { print "\$cardnumber ", Dumper $cardnumber; }
+    if ($debug) { print "\$borrower ", Dumper $borrower; }
+
+    # From C4::Circulation::AddIssue():
+    # $borrower is a hash with borrower informations (from GetMemberDetails).
+    # $barcode is the barcode of the item being issued.
+    # $datedue is a C4::Dates object for the max date of return, i.e. the date due (optional).
+    # $cancelreserve is 1 to override and cancel any pending reserves for the item (optional).
+    # $issuedate is the date to issue the item in iso (YYYY-MM-DD) format (optional).
+    # Defaults to today.  Unlike C<$datedue>, NOT a C4::Dates object, unfortunately.
+    my $datedue = C4::Circulation::AddIssue($borrower, $barcode->{'barcode'}, undef, undef, $date, undef);
+
+    if ($verbose) { print "\tBorrowernumber: " . $borrower->{'borrowernumber'} . " Barcode: " . $barcode->{'barcode'} . " Duedate: " . $datedue . "\n"; }
+
   }
 
-  # ($borrower) = &GetMemberDetails($borrowernumber, $cardnumber);
-
-  # From C4::Circulation::AddIssue:
-  # &AddIssue($borrower, $barcode, [$datedue], [$cancelreserve], [$issuedate])
-  # $borrower is a hash with borrower informations (from GetMemberDetails).
-  # $barcode is the barcode of the item being issued.
-  # $datedue is a C4::Dates object for the max date of return, i.e. the date due (optional).
-  # $cancelreserve is 1 to override and cancel any pending reserves for the item (optional).
-  # $issuedate is the date to issue the item in iso (YYYY-MM-DD) format (optional).
-  # Defaults to today.  Unlike C<$datedue>, NOT a C4::Dates object, unfortunately.
+  if ($i > $returns_after) {
+    # Do returns
+  }
 
   if ($verbose) { print_status(); }
 
 }
+
+# TODO Should all active issues be returned before we finnish? 
 
 ### SUBROUTINES ###
 
@@ -116,19 +132,21 @@ sub print_status {
 
 # Get commandline options
 sub get_options {
-  my $from = '';
-  my $max = '';
-  my $min = '';
-  my $verbose = '';
-  my $debug = '';
-  my $help = '';
+  my $from          = '';
+  my $max           = '';
+  my $min           = '';
+  my $returns_after = 0,
+  my $verbose       = '';
+  my $debug         = '';
+  my $help          = '';
 
-  GetOptions("f|from=s" => \$from,
-             "x|max=i" => \$max, 
-             "n|min=i" => \$min,
-             "v|verbose" => \$verbose,
-             "d|debug" => \$debug, 
-             "h|help" => \$help, 
+  GetOptions("f|from=s"    => \$from,
+             "x|max=i"     => \$max, 
+             "n|min=i"     => \$min,
+             "r|returns=i" => \$returns_after, 
+             "v|verbose"   => \$verbose,
+             "d|debug"     => \$debug, 
+             "h|help"      => \$help, 
              );
   
   pod2usage(-exitval => 0) if $help;
@@ -136,7 +154,7 @@ sub get_options {
   pod2usage( -msg => "\nMissing Argument: -x, --max required\n", -exitval => 1) if !$max;
   pod2usage( -msg => "\nMissing Argument: -n, --min required\n", -exitval => 1) if !$min;
 
-  return ($from, $max, $min, $verbose, $debug);
+  return ($from, $max, $min, $returns_after, $verbose, $debug);
 }       
 
 __END__
@@ -162,10 +180,14 @@ The first day to do simulations on. Iterates through all days to the present
 =item B<-n, --min>
 
 Minimum number of issues to do in a day
-                                                       
+
 =item B<-x, --max>
 
 Maximum number of issues to do in a day
+
+=item B<-r, --returns>
+
+Wait this many days before starting to do returns. Number of returns per day will be average of --min and --max. 
 
 =item B<-v, --verbose>
 
