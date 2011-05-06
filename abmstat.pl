cd07be71453021d5ddf0913b63797c930b3e3e49
Magnus Enger <magnus@enger.priv.no>
2011-05-06 10:29:49 +0200

    Fix holdings in abmstat.pl

--- abmstat.pl
+++ abmstat.pl
@@ -79,7 +79,7 @@ for (my $i=0; $i < $count; $i++) {
 	# Holdings
 	my $hold_query = "SELECT count(biblionumber)
 					FROM items 
-					WHERE YEAR(dateaccessioned) = $year
+					-- WHERE YEAR(dateaccessioned) = $year
 					AND homebranch = '" . $homebranch . "'
 					AND " . orify('itype', @{$itemtypes[$i]{'itypes'}});
 	my $hold_sth = $dbh->prepare($hold_query);
