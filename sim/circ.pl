a4b673a304366ab221e4d58f5bfc37fd64c6d0b2
Benjamin Rokseth <bensinober@gmail.com>
2013-10-22 09:26:46 +0200

    changed to stdlib YAML in circ.pl

--- sim/circ.pl
+++ sim/circ.pl
@@ -22,7 +22,7 @@ use C4::Context;
 use C4::Items;
 use C4::Members;
 use Date::Calc qw( Add_Delta_Days Day_of_Week Day_of_Week_to_Text Delta_Days Today Week_Number );
-use YAML::Tiny;
+use YAML qw(LoadFile);
 use Getopt::Long;
 use Pod::Usage;
 use Data::Dumper;
@@ -51,14 +51,14 @@ if ($zap) {
 }
 
 # Open the config
-my $yaml = YAML::Tiny->new;
+my $yaml = YAML->new;
 if (-e $config) {
-  $yaml = YAML::Tiny->read($config);
+  $yaml = YAML::LoadFile($config);
 } else {
   die "Could not find $config\n";
 }
-my $min = $yaml->[0]->{min_issues_per_day};
-my $max = $yaml->[0]->{max_issues_per_day};
+my $min = $yaml->{min_issues_per_day};
+my $max = $yaml->{max_issues_per_day};
 
 print "\nStarting circ.pl\nSettings:\n"      if $verbose;
 print "Simulating circulations from $from\n" if $verbose;
@@ -121,8 +121,8 @@ for ( my $i = 0; $i <= $j; $i++ ) {
   my $current_max = $max;
 
   # 1. Should we alter the default based on the year?
-  if ($yaml->[0]->{years}->{$year}) {
-    my $ratio = $yaml->[0]->{years}->{$year};
+  if ($yaml->{years}->{$year}) {
+    my $ratio = $yaml->{years}->{$year};
     $current_min = int ( ( $min * $ratio ) / 100 );
     $current_max = int ( ( $max * $ratio ) / 100 );
     if ($verbose) { print "Altering min/max based on year $year: min = $current_min, max = $current_max\n"; }
@@ -130,8 +130,8 @@ for ( my $i = 0; $i <= $j; $i++ ) {
   
   # 2. Should we alter the default based on the number of the week?
   my $week_number = Week_Number($year, $month, $day);
-  if ($yaml->[0]->{weeks}->{$week_number}) {
-    my $ratio = $yaml->[0]->{weeks}->{$week_number};
+  if ($yaml->{weeks}->{$week_number}) {
+    my $ratio = $yaml->{weeks}->{$week_number};
     $current_min = int ( ( $min * $ratio ) / 100 );
     $current_max = int ( ( $max * $ratio ) / 100 );
     if ($verbose) { print "Altering min/max based on week #$week_number: min = $current_min, max = $current_max\n"; }
@@ -139,8 +139,8 @@ for ( my $i = 0; $i <= $j; $i++ ) {
   
   # 3. Should we alter the default based on the day of the week?
   my $day_of_week = lc Day_of_Week_to_Text(Day_of_Week($year, $month, $day), 1); # 1 = English
-  if ($yaml->[0]->{days}->{$day_of_week}) {
-    my $ratio = $yaml->[0]->{days}->{$day_of_week};
+  if ($yaml->{days}->{$day_of_week}) {
+    my $ratio = $yaml->{days}->{$day_of_week};
     $current_min = int ( ( $current_min * $ratio ) / 100 );
     $current_max = int ( ( $current_max * $ratio ) / 100 );
     if ($verbose) { print "Altering min/max based on $day_of_week: min = $current_min, max = $current_max\n"; }
@@ -167,7 +167,7 @@ for ( my $i = 0; $i <= $j; $i++ ) {
     
     # Find the number of issues to do for this patron
     # Random number between min and max
-    my $issues_to_do_for_patron = int(rand($yaml->[0]->{'max_issues_per_day_per_borrower'} - $yaml->[0]->{'min_issues_per_day_per_borrower'}+1)) + $yaml->[0]->{'min_issues_per_day_per_borrower'};
+    my $issues_to_do_for_patron = int(rand($yaml->{'max_issues_per_day_per_borrower'} - $yaml->{'min_issues_per_day_per_borrower'}+1)) + $yaml->{'min_issues_per_day_per_borrower'};
     print "\tGoing to do $issues_to_do_for_patron issues for patron " . $borrowerid->{'borrowernumber'} . ", branch " . $borrower->{'branchcode'} . "\n";
 
     # AddIssue() accesses userenv so we need to create it
@@ -177,7 +177,7 @@ for ( my $i = 0; $i <= $j; $i++ ) {
     # We set the branch of the librarian to that of the borrower we
     # have already selected, so that the issue is made from the 
     # branch the partron is connected to. 
-    C4::Context::set_userenv($yaml->[0]->{staff_user}, undef, undef, undef, undef, $borrower->{'branchcode'}, undef, undef, undef, undef);
+    C4::Context::set_userenv($yaml->{staff_user}, undef, undef, undef, undef, $borrower->{'branchcode'}, undef, undef, undef, undef);
     
     # Get the barcode of x random items that are not on loan
     $get_barcodes_sth->execute($borrower->{'branchcode'}, $issues_to_do_for_patron);
